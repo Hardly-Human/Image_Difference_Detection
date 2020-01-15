c708b028dd267bcd2feceea530033f908e4e577a
Shaik Rehanuddin <32925391+rehan1024@users.noreply.github.com>
2020-01-15 17:45:02 +0530

    image difference

---
+++ image_diff.py
@@ -0,0 +1,13 @@
+# import the necessary packages
+from skimage.measure import compare_ssim
+import argparse
+import imutils
+import cv2
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-f", "--first", required=True,
+	help="first input image")
+ap.add_argument("-s", "--second", required=True,
+	help="second")
+args = vars(ap.parse_args())
